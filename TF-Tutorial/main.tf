db5469cc80511ff4095619291bcc70f30c6ae8c5
zarina-a-meeran <60441603+zarina-a-meeran@users.noreply.github.com>
2020-07-30 09:43:33 +0100

    Add files via upload

---
+++ TF-Tutorial/main.tf
@@ -0,0 +1,36 @@
+provider "azurerm" {
+  version = "=1.44.0"
+  subscription_id = "8b150e3c-1d0c-453b-86a8-12110c26d98a" 
+}
+
+# Create a new resource group
+resource "azurerm_resource_group" "rg" {
+    name     = "${var.prefix}rg"
+    location = "westeurope"
+}
+
+resource "azurerm_app_service_plan" "asp" {
+  name = "${var.prefix}-asp"
+  location = "westeurope"
+  resource_group_name = "${azurerm_resource_group.rg.name}"
+  kind                = "Linux"
+  reserved            = true
+
+  sku {
+    tier = "Basic"
+    size = "B1"
+  }
+}
+
+resource "azurerm_app_service" "as" {
+  name                = "${var.prefix}-appservice"
+  location = "westeurope"
+  resource_group_name = "${azurerm_resource_group.rg.name}"
+  app_service_plan_id = "${azurerm_app_service_plan.asp.id}"
+
+  site_config {
+    linux_fx_version = "PHP|7.0"
+    scm_type         = "LocalGit"
+  }
+}
+
